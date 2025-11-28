7c5ca6a579f527d8d8433ff11bcf0fbd13488f97
phoenix marie <phoenixmariepornstaractress@gmail.com>
2025-11-28 17:07:41 +0330

    Update PaymentService.kt

--- src/main/java/com/example/zarinpal/data/remote/PaymentService.kt
+++ src/main/java/com/example/zarinpal/data/remote/PaymentService.kt
@@ -17,109 +17,57 @@ import com.example.zarinpal.data.remote.dto.verification.PaymentVerificationData
 import com.example.zarinpal.data.remote.dto.verification.PaymentVerifyRequest
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
-import io.ktor.client.features.defaultRequest
-import io.ktor.client.features.json.JsonFeature
-import io.ktor.client.features.json.serializer.KotlinxSerializer
-import io.ktor.client.features.logging.LogLevel
-import io.ktor.client.features.logging.Logging
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.header
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
+import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
-import android.os.Build
-import androidx.core.os.BuildCompat
-import androidx.core.os.BundleCompat
-
 
 /**
- * PaymentService interface provides functions to interact with the ZarinPal payment system.
- * Each function corresponds to a specific endpoint in the ZarinPal API.
+ * Defines operations available for interacting with ZarinPal API.
  */
 interface PaymentService {
 
-    /**
-     * Creates a payment request.
-     *
-     * @param paymentRequest The payment request details.
-     * @return A response containing the payment data or null if the request fails.
-     */
-    suspend fun createPayment(paymentRequest: CreatePaymentRequest): CreatePaymentDataResponse?
-
-    /**
-     * Creates a payment request.
-     *
-     * @param paymentRequest The payment request details.
-     * @return A response containing the payment data or null if the request fails.
-     */
-    suspend fun paymentVerify(paymentVerifyRequest: PaymentVerifyRequest): PaymentVerificationDataResponse?
-
-    /**
-     * Inquires about a payment.
-     *
-     * @param paymentInquiryRequest The request containing the payment inquiry data.
-     * @return A response containing the payment inquiry data or null if the inquiry fails.
-     */
-    suspend fun paymentInquiry(paymentInquiryRequest: PaymentInquiryRequest): PaymentInquiryDataResponse?
-
-    /**
-     * Retrieves unverified payments.
-     *
-     * @param paymentUnVerifiedRequest The request containing the unverified payment data.
-     * @return A response containing the unverified payment data or null if the request fails.
-     */
-    suspend fun paymentUnVerified(paymentUnVerifiedRequest: PaymentUnVerifiedRequest): PaymentUnVerifiedDataResponse?
-
-    /**
-     * Reverses a payment.
-     *
-     * @param paymentReverseRequest The request containing the payment reversal data.
-     * @return A response containing the payment reversal data or null if the reversal fails.
-     */
-    suspend fun paymentReverse(paymentReverseRequest: PaymentReverseRequest): PaymentReverseDataResponse?
-
-    /**
-     * Retrieves a list of transactions.
-     *
-     * @param transactionRequest The request containing the transaction data.
-     * @return A list of transactions or null if the request fails.
-     */
-    suspend fun getTransactions(transactionRequest: TransactionRequest): List<Session>?
-
-    /**
-     * Processes a payment refund.
-     *
-     * @param paymentRefundRequest The request containing the payment refund data.
-     * @return A response containing the payment refund data or null if the refund fails.
-     */
-    suspend fun paymentRefund(paymentRefundRequest: PaymentRefundRequest): PaymentRefundResponse?
+    suspend fun createPayment(request: CreatePaymentRequest): CreatePaymentDataResponse?
+    suspend fun paymentVerify(request: PaymentVerifyRequest): PaymentVerificationDataResponse?
+    suspend fun paymentInquiry(request: PaymentInquiryRequest): PaymentInquiryDataResponse?
+    suspend fun paymentUnVerified(request: PaymentUnVerifiedRequest): PaymentUnVerifiedDataResponse?
+    suspend fun paymentReverse(request: PaymentReverseRequest): PaymentReverseDataResponse?
+    suspend fun getTransactions(request: TransactionRequest): List<Session>?
+    suspend fun paymentRefund(request: PaymentRefundRequest): PaymentRefundResponse?
 
     companion object {
-        /**
-         * Creates an instance of PaymentService with the provided configuration.
-         *
-         * @param config The configuration for the service.
-         * @return A new instance of PaymentService.
-         */
+
         fun create(config: Config): PaymentService {
+
+            val client = HttpClient(Android) {
+
+                install(Logging) {
+                    level = LogLevel.ALL
+                }
+
+                install(ContentNegotiation) {
+                    json(
+                        Json {
+                            ignoreUnknownKeys = true
+                            prettyPrint = false
+                        }
+                    )
+                }
+
+                defaultRequest {
+                    header("User-Agent", "ZarinPalSdk/v1.0.1 (android-kotlin)")
+                    contentType(ContentType.Application.Json)
+                }
+            }
+
             return PaymentServiceImpl(
                 config = config,
-                client = HttpClient(Android) {
-                    install(Logging) {
-                        level = LogLevel.ALL
-                    }
-                    install(JsonFeature) {
-                        serializer = KotlinxSerializer(
-                            Json {
-                                ignoreUnknownKeys = true
-                            }
-                        )
-                    }
-                    defaultRequest {
-                        header("User-Agent", "ZarinPalSdk/v.1.0.1 (android kotlin)")
-                        header("Content-Type", "application/json")
-                        contentType(ContentType.Application.Json)
-                    }
-                },
+                client = client
             )
         }
     }
